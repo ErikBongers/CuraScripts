502024bc788377deb04db6fa13c9dbe2e05e1584
Erik Bongers <mail@erikbongers.net>
2019-07-06 12:11:59 +0200

    Initial version.

---
+++ DisplayFileAndLayersV2.py
@@ -0,0 +1,57 @@
+# Cura PostProcessingPlugin
+# Author: Erik Bongers - based on DisplayFilenameAndLayerOnLCD by Amanda de Castilho
+# Date:     July 5, 2019
+
+# Description:  This plugin inserts a line at the start of each layer
+#               The line contains
+#                   - a user-defined label or the filename
+#                   - the current layer
+#                   - total number of layers.
+#               The G-code M117 is used to display the full message.
+# TODO:
+#
+from ..Script import Script
+from UM.Application import Application
+
+class DisplayFileAndLayersV2(Script):
+    def __init__(self):
+        super().__init__()
+
+    def getSettingDataString(self):
+        return """{
+            "name": "Display filename and layer V2",
+            "key": "DisplayFileAndLayersV2",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "name":
+                {
+                    "label": "text to display:",
+                    "description": "By default the current filename will be displayed on the LCD. Enter text here to override the filename and display something else.",
+                    "type": "str",
+                    "default_value": ""
+                }
+            }
+        }"""
+    
+    def execute(self, data):
+        if self.getSettingValueByKey("name") != "":
+            name = self.getSettingValueByKey("name")
+        else:
+            name = Application.getInstance().getPrintInformation().jobName       
+        i = 1
+        for layer in data:
+            txtLayers = str(i) + "/" + str(len(data))
+            display_text = "M117 " + name[0:20-len(txtLayers)] + " " + txtLayers
+            layer_index = data.index(layer)
+            lines = layer.split("\n")
+            for line in lines:
+                if line.startswith(";LAYER:"):
+                    line_index = lines.index(line)
+                    lines.insert(line_index + 1, display_text)
+                    i += 1
+            final_lines = "\n".join(lines)
+            data[layer_index] = final_lines
+            
+        return data
--- README.md
+++ README.md
@@ -3,6 +3,13 @@ Extension scripts for the Cura slicer (3D printing)
 
 ## DisplayFileAndLayersV2
 This script is an alternative to (and based on) the original DisplayFilenameAndLayerOnLCD.py script by Amanda de Castilho, that comes preinstalled with Cura.
+It shows the following at the LCD screen:
+  - The filename or a user text
+  - The current layer being printed
+  - The total number of layers
+The length of the full text line is limited to 21 characters, which is the limit of the LCD on an Ender 3 printer.
+The filename or user text is trimmed to accomodate this, so that the layer numbers are always in view.
+The layer numbering starts at 1, as opposed to 0-based indexing. (not all 3D-printer users are programmers :) )
 
 The script is tested with Cura 4.1.0, but will probably work with version 3 as well.
 
